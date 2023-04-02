010a5fda12311bfa58247e6102c149c42586882e
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-02 20:07:46 +0200

    modify brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-from brain_games.greeting import greet_user
-from brain_games.brain_even_code import comp_instruction, comp_decision
+from brain_games.brain_even_code import comp_decision
+
 
 def main():
-    greet_user()
+    comp_decision()
 
 
-    if __name__ == '__main__':
-        main()
-        comp_instruction()
-        comp_decision()
+if __name__ == '__main__':
+    main()
