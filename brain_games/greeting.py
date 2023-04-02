71c129cdbaf23276e7f1cda2ce2428058dc30012
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-02 20:06:25 +0200

    modify greeting.py

--- brain_games/greeting.py
+++ brain_games/greeting.py
@@ -1,10 +1,12 @@
-# This function welcomes the user, asks for his/her name and greets him/her.
-# This function is the same for all games.
+#################################################
+# This block of code welcomes the user and asks for his/her name.
+# This code is the same for all brain games.
 
 import prompt
 
 
-def greet_user():
-    print('Welcome to the Brain Games')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+print('Welcome to the Brain Games')
+
+name = prompt.string('May I have your name? ')
+
+print(f'Hello, {name}!')
