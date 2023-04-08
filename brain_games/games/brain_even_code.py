c794b8e864a4373cac32e936aef8a1ee566d9fdf
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-08 16:35:25 +0200

    modify brain_even_code.py

--- brain_games/games/brain_even_code.py
+++ brain_games/games/brain_even_code.py
@@ -1,4 +1,4 @@
-from brain_games.all_games_code import comp_rand_num, user_response, name
+from brain_games.games.all_games_code import comp_rand_num, user_response, name
 
 #################################################
 # This block of code instructs the user on how to play the game.
