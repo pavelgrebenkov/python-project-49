94af44790083eac552a8c0a44c89d99dada266b7
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-08 15:34:07 +0200

    delete cli.py

--- brain_games/cli.py
+++
@@ -1,9 +0,0 @@
-# This function welcomes the user and asks for his/her name
-
-import prompt
-
-
-def welcome_user():
-    print('Welcome to the Brain Games')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
