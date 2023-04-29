a948e317fc0d3ef0d70a8bb400091b1076b16e89
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-29 18:53:19 +0200

    modify brain_gcd_code.py

--- brain_games/games/brain_gcd_code.py
+++ brain_games/games/brain_gcd_code.py
@@ -12,33 +12,39 @@ print(instruction)
 # This block of code makes decisions about the user’s responses.
 
 
+def comp_question():
+    rand_num_1 = comp_rand_num()
+    rand_num_2 = comp_rand_num()
+    rand_num_pair = f'{rand_num_1} {rand_num_2}'
+    question = f'Question: {rand_num_pair}'
+    print(question)
+    rand_num_pair_gcd = math.gcd(rand_num_1, rand_num_2)
+    return rand_num_pair_gcd
+
+
 def comp_decision():
     attempts_correct = 0
 
     while attempts_correct < 3:
-        rand_num_1 = comp_rand_num()
-        rand_num_2 = comp_rand_num()
-        rand_num_pair = f'{rand_num_1} {rand_num_2}'
-        question = f'Question: {rand_num_pair}'
-        print(question)
-
-        rand_num_pair_gcd = math.gcd(rand_num_1, rand_num_2)
+        rand_num_pair_gcd = comp_question()
 
         try:
             player_response = int(user_response())
 
         except ValueError:
             print(f"Your answer isn't clear. ;(. Let's try again {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
         if rand_num_pair_gcd == player_response:
             attempts_correct += 1
             print("Correct!")
 
-        if rand_num_pair_gcd != player_response:
+        else:
             print(f"""{player_response} is wrong answer ;(.
 Correct answer was {rand_num_pair_gcd}.
 Let's try again, {name}!""")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
     print(f"Congratuations, {name}!")
