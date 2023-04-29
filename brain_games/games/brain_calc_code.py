b86bf6f2a68a2f7b5db87508c417b65b2060129e
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-29 18:50:25 +0200

    modify brain_calc_code.py

--- brain_games/games/brain_calc_code.py
+++ brain_games/games/brain_calc_code.py
@@ -12,34 +12,40 @@ print(instruction)
 # This block of code makes decisions about the user’s responses.
 
 
+def comp_question():
+    rand_num = comp_rand_num()
+    math_opr = ('+', '-', '*')
+    rand_math_opr = random.choice(math_opr)
+    rand_math_expr = f'{rand_num} {rand_math_opr} {rand_num}'
+    question = f'Question: {rand_math_expr}'
+    print(question)
+    math_expr_result = eval(rand_math_expr)
+    return math_expr_result
+
+
 def comp_decision():
     attempts_correct = 0
 
     while attempts_correct < 3:
-        rand_num = comp_rand_num()
-        math_opr = ('+', '-', '*')
-        rand_math_opr = random.choice(math_opr)
-        rand_math_expr = f'{rand_num} {rand_math_opr} {rand_num}'
-        question = f'Question: {rand_math_expr}'
-        print(question)
-
-        math_expr_result = eval(rand_math_expr)
+        math_expr_result = comp_question()
 
         try:
             player_response = int(user_response())
 
         except ValueError:
             print(f"Your answer isn't clear. ;(. Let's try again {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
         if math_expr_result == player_response:
             attempts_correct += 1
             print("Correct!")
 
-        if math_expr_result != player_response:
+        else:
             print(f"""{player_response} is wrong answer ;(.
 Correct answer was {math_expr_result}.
 Let's try again, {name}!""")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
     print(f"Congratuations, {name}!")
