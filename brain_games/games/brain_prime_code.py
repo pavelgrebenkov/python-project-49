74aeafebaffd4403c117a5e9436c5c195c8df4ab
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-29 18:54:07 +0200

    modify brain_prime_code.py

--- brain_games/games/brain_prime_code.py
+++ brain_games/games/brain_prime_code.py
@@ -12,46 +12,46 @@ print(instruction)
 # This block of code makes decisions about the user’s responses.
 
 
+def comp_question():
+    rand_num = comp_rand_num()
+    question = f'Question: {rand_num}'
+    print(question)
+
+    for i in range(2, rand_num):
+        if (rand_num % i) == 0:
+            return False
+    else:
+        return True
+
+
 def comp_decision():
     attempts_correct = 0
 
     while attempts_correct < 3:
-        rand_num = comp_rand_num()
-        question = f'Question: {rand_num}'
-        print(question)
-
-        prime_num = True
-        non_prime_num = False
-
-        def is_prime():
-            if rand_num > 1:
-                for i in range(2, rand_num):
-                    if (rand_num % i) == 0:
-                        return non_prime_num
-                else:
-                    return prime_num
-            else:
-                return non_prime_num
+        prime_num = comp_question()
 
         player_response = user_response()
 
-        yes = player_response.lower() == "yes"
-        no = player_response.lower() == "no"
+        yes = bool(player_response.lower() == "yes")
+        no = bool(player_response.lower() == "no")
 
-        if (is_prime() == yes and is_prime() != no):
+        if (prime_num == yes and prime_num != no):
             attempts_correct += 1
             print("Correct!")
 
-        elif (is_prime() is True and is_prime() == no):
+        elif (prime_num is True and no):
             print(f"No is wrong answer ;(. Let's try again, {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
-        elif (is_prime() is False and is_prime() != yes):
+        elif (prime_num is False and yes):
             print(f"Yes is wrong answer ;(. Let's try again, {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
         else:
             print(f"Your answer isn't clear. ;(. Let's try again, {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
     print(f"Congratuations, {name}!")
