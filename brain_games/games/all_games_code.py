adda9799131b14912db16e6abdafa31aec4007b9
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-30 19:12:47 +0200

    modify all_games_code.py

--- brain_games/games/all_games_code.py
+++ brain_games/games/all_games_code.py
@@ -9,7 +9,7 @@ import random
 
 
 def welcome_user():
-    print('Welcome to the Brain Games')
+    print('Welcome to the Brain Games!')
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
