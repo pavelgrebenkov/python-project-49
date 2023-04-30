08761e2fccf6d626968c2edb9d2a664160a7b533
pavelgrebenkov <pavellearnnow@gmail.com>
2023-04-30 19:13:20 +0200

    modify brain_progression_code.py

--- brain_games/games/brain_progression_code.py
+++ brain_games/games/brain_progression_code.py
@@ -12,39 +12,43 @@ print(instruction)
 # This block of code makes decisions about the user’s responses.
 
 
+def comp_question():
+    rand_math_seq = []
+    rand_num = comp_rand_num()
+    for i in range(1, rand_num + 1):
+        rand_math_seq.append(rand_num + (i - 1) * rand_num)
+    elem = ".."
+    rand_index = random.randrange(len(rand_math_seq))
+    correct_answer = rand_math_seq[rand_index]
+    rand_math_seq[rand_index] = elem
+    question = f'Question: {str(rand_math_seq)[1 : -1]}'
+    print(question)
+    return correct_answer
+
+
 def comp_decision():
     attempts_correct = 0
 
     while attempts_correct < 3:
-        rand_math_seq = []
-        rand_num = comp_rand_num()
-
-        for i in range(1, rand_num + 1):
-            rand_math_seq.append(rand_num + (i - 1) * rand_num)
-
-        elem = ".."
-        rand_index = random.randrange(len(rand_math_seq))
-        correct_answer = rand_math_seq[rand_index]
-        rand_math_seq[rand_index] = elem
-
-        question = f'Question: {str(rand_math_seq)[1 : -1]}'
-        print(question)
+        correct_answer = comp_question()
 
         try:
             player_response = int(user_response())
 
         except ValueError:
             print(f"Your answer isn't clear. ;(. Let's try again {name}!")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
         if correct_answer == player_response:
             attempts_correct += 1
             print("Correct!")
 
-        if correct_answer != player_response:
+        else:
             print(f"""{player_response} is wrong answer ;(.
 Correct answer was {correct_answer}.
 Let's try again, {name}!""")
-            return comp_decision()
+            attempts_correct = 0
+            continue
 
     print(f"Congratuations, {name}!")
