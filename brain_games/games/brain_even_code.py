9c4fe165a8da299d4e95689944fbaf2811d1a272
pavelgrebenkov <pavellearnnow@gmail.com>
2023-05-03 20:01:55 +0200

    modify brain_even_code.py

--- brain_games/games/brain_even_code.py
+++ brain_games/games/brain_even_code.py
@@ -36,17 +36,20 @@ def comp_decision():
             print("Correct!")
 
         elif (num_even is False and yes):
-            print(f"Yes is wrong answer ;(. Let's try again, {name}!")
+            print(f"""'yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name}!""")
             attempts_correct = 0
             continue
 
         elif (num_even is True and no):
-            print(f"No is wrong answer ;(. Let's try again, {name}!")
+            print(f"""'no' is wrong answer ;(. Correct answer was 'yes'.
+Let's try again, {name}!""")
             attempts_correct = 0
             continue
 
         else:
-            print(f"Your answer isn't clear. ;(. Let's try again, {name}!")
+            print(f"""Your answer isn't clear. ;(.
+Let's try again, {name}!""")
             attempts_correct = 0
             continue
 
