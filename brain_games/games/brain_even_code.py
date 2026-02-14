359b3d04570cbb93eb5e78569e8460e437913eff
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-14 18:43:28 +0400

    modify brain_even_code.py

--- brain_games/games/brain_even_code.py
+++ brain_games/games/brain_even_code.py
@@ -1,15 +1,4 @@
-from brain_games.games.all_games_code import comp_rand_num, user_response, name
-
-#################################################
-# This block of code instructs the user on how to play the game.
-
-
-instruction = 'Answer "yes" if the number is even, otherwise answer "no".'
-print(instruction)
-
-
-#################################################
-# This block of code makes decisions about the user’s responses.
+from brain_games.games.all_games_code import comp_rand_num, user_response
 
 
 def comp_question():
@@ -19,7 +8,10 @@ def comp_question():
     return rand_num
 
 
-def comp_decision():
+def comp_decision(name):
+    instruction = 'Answer "yes" if the number is even, otherwise answer "no".'
+    print(instruction)
+
     attempts_correct = 0
 
     while attempts_correct < 3:
@@ -36,19 +28,19 @@ def comp_decision():
             print("Correct!")
 
         elif (num_even is False and yes):
-            print(f"""'yes' is wrong answer ;(. Correct answer was 'no'.
+            print(f"""'yes' is wrong answer. Correct answer was 'no'.
 Let's try again, {name}!""")
             # attempts_correct = 0
             return
 
         elif (num_even is True and no):
-            print(f"""'no' is wrong answer ;(. Correct answer was 'yes'.
+            print(f"""'no' is wrong answer. Correct answer was 'yes'.
 Let's try again, {name}!""")
             # attempts_correct = 0
             return
 
         else:
-            print(f"""Your answer isn't clear. ;(.
+            print(f"""Your answer isn't clear.
 Let's try again, {name}!""")
             # attempts_correct = 0
             return
