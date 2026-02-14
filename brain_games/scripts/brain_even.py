a5f3907a9d50bf7a9c5c0db088ec2d84efb7a3a7
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-14 18:45:49 +0400

    modify brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from brain_games.games.cli import welcome_user
 from brain_games.games.brain_even_code import comp_decision
 
 
 def main():
-    comp_decision()
+    name = welcome_user()
+    comp_decision(name)
 
 
 if __name__ == '__main__':
