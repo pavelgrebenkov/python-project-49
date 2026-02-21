6231d8d8574acfde8b8b4ae1a91a06ef9dc8ce8f
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-21 21:22:19 +0400

    refactor: rename brain_even_code.py to even.py
    even.py provides game instructions, question and answer; game-flow logic moved to engine.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,12 @@
+# even.py
+from brain_games.games.utils import generate_number
+
+ANSWER_TYPE = 'yesno'  
+INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def generate_round():
+    number = generate_number()
+    question = str(number)
+    correct_answer = "yes" if number % 2 == 0 else "no"
+    return (question, correct_answer)
+
