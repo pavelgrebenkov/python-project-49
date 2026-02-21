cbedf19b0bd0f05c7900a0f2a434552625c6bd90
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-21 21:37:22 +0400

    refactor: updated brain_even.py
    brain_even.py now calls the main engine function run_game()

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,13 @@
+# brain_even.py
 #!/usr/bin/env python3
 
 from brain_games.games.cli import welcome_user
-from brain_games.games.brain_even_code import comp_decision
-
+from brain_games.games import even
+from brain_games.games.engine import run_game
 
 def main():
     name = welcome_user()
-    comp_decision(name)
-
+    run_game(even, name)
 
 if __name__ == '__main__':
     main()
