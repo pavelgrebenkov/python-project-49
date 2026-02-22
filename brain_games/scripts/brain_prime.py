7ddcb3be71ece6f588ac2a669bbaaf2d4c5ec930
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-22 14:45:56 +0400

    refactor: updated brain_prime.py
    brain_prime.py has been modified to pass the prime game module and user name to run_game

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,13 +1,14 @@
+# brain_prime.py
 #!/usr/bin/env python3
 
 from brain_games.games.cli import welcome_user
-from brain_games.games.brain_prime_code import comp_decision
-
+from brain_games.games import prime
+from brain_games.games.engine import run_game
 
 def main():
     name = welcome_user()
-    comp_decision(name)
-
+    run_game(prime, name)
 
 if __name__ == '__main__':
     main()
+
