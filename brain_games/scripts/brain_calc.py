ec8c5ea311ad72fb4b3c5cb12e362be7bf754513
pavel_grebenkov <pavellearnnow@gmail.com>
2026-02-22 15:04:49 +0400

    refactor: update brain_calc.py
    brain_calc.py passes the calc game module and user name to run_game in engine.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,13 @@
+# brain_calc.py
 #!/usr/bin/env python3
 
 from brain_games.games.cli import welcome_user
-from brain_games.games.brain_calc_code import comp_decision
-
+from brain_games.games import calc
+from brain_games.games.engine import run_game
 
 def main():
     name = welcome_user()
-    comp_decision(name)
-
+    run_game(calc, name)
 
 if __name__ == '__main__':
     main()
